ec50f76f2a8fcb23952dbaa2c47c261c3e8d9e25
Michael VanDaniker <mvandani@gmail.com>
2019-12-15 15:01:47 -0500

    Adds whitelisting to the contract

--- src/contracts/Pixie.sol
+++ src/contracts/Pixie.sol
@@ -2,19 +2,65 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract Pixie {
 
+  address public owner;
+
+  bool public active = true;
+
+  mapping(address => bool) public allowedAddresses;
+
   uint8 constant numColumns = 16;
   uint8 constant numRows = 16;
-
   uint24[numColumns * numRows] public colors;
 
   event ColorSetEvent(uint8 row, uint8 column, uint24 color);
 
   constructor() public {
+    owner = msg.sender;
+    grantAccess(owner);
+
+    // Set the initial colors for each pixel
     for(uint8 i = 0; i < numRows; i++) {
       for(uint8 j = 0; j < numColumns; j++) {
-        setColor(i, j, 0xffffff);
+        // Do an inline assignment of the colors rather than calling setColor so we don't emit events during initialization
+        colors[i * numColumns + j] = 0xfcfcfc;
       }
     }
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "sender must be owner");
+    _;
+  }
+
+  modifier onlyWhenActive() {
+    require(active, "Pixie is not active");
+    _;
+  }
+
+  modifier onlyWhenPaused() {
+    require(!active, "Pixie is not paused");
+    _;
+  }
+
+  function pause() public onlyOwner onlyWhenActive {
+    active = false;
+  }
+
+  function resume() public onlyOwner onlyWhenPaused {
+    active = true;
+  }
+
+  function grantAccess(address account) public onlyOwner {
+    allowedAddresses[account] = true;
+  }
+
+  function revokeAccess(address account) public onlyOwner {
+    require(account != owner, "The owner's access cannot be revoked");
+    delete allowedAddresses[account];
+  }
+
+  function hasAccess(address account) public view returns (bool allowed) {
+    return allowedAddresses[account] == true;
   }
 
   function getAllColors() public view returns (uint24[numColumns * numRows] memory _colors) {
@@ -25,8 +71,8 @@ contract Pixie {
     return colors[row * numColumns + column];
   }
 
-
-  function setColor(uint8 row, uint8 column, uint24 color) public {
+  function setColor(uint8 row, uint8 column, uint24 color) public onlyWhenActive {
+    require(hasAccess(msg.sender), "Address is not allowed to call setColor");
     colors[row * numColumns + column] = color;
 
     emit ColorSetEvent(row, column, color);
--- test/Pixie.test.js
+++ test/Pixie.test.js
@@ -1,28 +1,112 @@
 const PixieContract = artifacts.require("./Pixie.sol");
 
+const expectThrow = async promise => {
+  try {
+    await promise;
+  } catch (error) {
+    // TODO: Check jump destination to destinguish between a throw
+    //       and an actual invalid jump.
+    const invalidJump = error.message.search('invalid JUMP') >= 0;
+    // TODO: When we contract A calls contract B, and B throws, instead
+    //       of an 'invalid jump', we get an 'out of gas' error. How do
+    //       we distinguish this from an actual out of gas event? (The
+    //       testrpc log actually show an 'invalid jump' event.)
+    const outOfGas = error.message.search('out of gas') >= 0;
+    const exited = error.message.search('exited with an error') >= 0;
+    assert(
+      invalidJump || outOfGas || exited,
+      "Expected throw, got '" + error + "' instead",
+    );
+    return;
+  }
+  assert.fail('Expected throw not received');
+};
+
 contract("Pixie", accounts => {
   let pixie;
+  const DEFAULT_COLOR = 0xfcfcfc;
 
   beforeEach(async () => {
     pixie = await PixieContract.new();
   })
 
   it("has initial values of 0", async () => {
-    const c = await pixie.getColor(0, 0);
-    assert.equal(c, 0xffffff);
+    assert.equal(await pixie.getColor(0, 0), DEFAULT_COLOR);
   });
 
   it("writes a color", async () => {
     await pixie.setColor(0, 0, 0xff0000, {from: accounts[0]});
-    const c = await pixie.getColor(0, 0);
-    
-    assert.equal(c, 0xff0000);
+    assert.equal(await pixie.getColor(0, 0), 0xff0000);
   });
 
   it("returns all colors", async () => {
     const c = await pixie.getAllColors();
-
     assert.equal(c.length, 256);
-    assert.equal(c[0], 0xffffff);
+    assert.equal(c[0], DEFAULT_COLOR);
+  });
+
+  it("gives the owner access initially", async () => {
+    assert.isTrue(await pixie.hasAccess(accounts[0]));
+  });
+
+  it("does not give a random account access initially or let them paint without access", async () => {
+    assert.isFalse(await pixie.hasAccess(accounts[1]));
+    assert.equal(await pixie.getColor(0, 0), DEFAULT_COLOR);
+    await expectThrow(
+      pixie.setColor(0, 0, 0xcc00cc, {from: accounts[1]})
+    );
+    assert.equal(await pixie.getColor(0, 0), DEFAULT_COLOR);
+  });
+
+  it("allows the owner to grants an account access, allowing painting, and allows the owner to revoke access, denying painting", async () => {
+    // Precondition
+    assert.isFalse(await pixie.hasAccess(accounts[1]));
+
+    // Give an account access
+    await pixie.grantAccess(accounts[1], {from: accounts[0]});
+    
+    // Test that the account can paint
+    assert.isTrue(await pixie.hasAccess(accounts[1]));
+    assert.equal(await pixie.getColor(0, 0), DEFAULT_COLOR);
+    await pixie.setColor(0, 0, 0xdd00dd, {from: accounts[1]});
+    assert.equal(await pixie.getColor(0, 0), 0xdd00dd);
+
+    // Revoke access
+    await pixie.revokeAccess(accounts[1], {from: accounts[0]});
+
+    // Test that the account can no longer paint
+    assert.isFalse(await pixie.hasAccess(accounts[1]));
+    await expectThrow(
+      pixie.setColor(0, 0, 0xee00ee, {from: accounts[1]})
+    );
+    assert.equal(await pixie.getColor(0, 0), 0xdd00dd);
+  });
+
+  it("only allows painting when active", async () => {
+    // Preconditions
+    assert.isTrue(await pixie.active());
+    assert.equal(await pixie.getColor(0, 0), DEFAULT_COLOR);
+
+    // Test that we can paint when active
+    await pixie.setColor(0, 0, 0xbb00bb, {from: accounts[0]});
+    assert.equal(await pixie.getColor(0, 0), 0xbb00bb);
+
+    // Pause
+    await pixie.pause({from: accounts[0]});
+    assert.isFalse(await pixie.active());
+
+    // Test that we can't paint when not active
+    await expectThrow(
+      pixie.setColor(0, 0, 0xff0000, {from: accounts[0]})
+    );
+    assert.equal(await pixie.getColor(0, 0), 0xbb00bb);
+
+    // Resume
+    await pixie.resume({from: accounts[0]});
+    assert.isTrue(await pixie.active());
+
+    // Test that we can paint again
+    await pixie.setColor(0, 0, 0x990000, {from: accounts[0]});
+    assert.equal(await pixie.getColor(0, 0), 0x990000);
   });
 });
